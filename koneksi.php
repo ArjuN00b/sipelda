e116c95ceca0d063eac7294e05fca95d044bbdab
MUHAMMAD FAKHRI ANSHARY YUSAF <fakhrianshary@gmail.com>
2026-05-25 12:18:23 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,12 @@
+<?php
+$host     = "localhost";
+$user     = "root";
+$password = "";    
+$db       = "db_aplikasi";
 
+$koneksi = mysqli_connect($host, $user, $password, $db);
+
+if (!$koneksi) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+?>
