c71dad9c6fd5133b6d1c5df2a905e70626b286b1
MUHAMMAD FAKHRI ANSHARY YUSAF <fakhrianshary@gmail.com>
2026-05-27 09:41:17 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,1 +1,24 @@
+<?php
+session_start();
 
+// Validasi sesi: Apakah sudah login dan apakah rolenya admin?
+if (!isset($_SESSION['status']) || $_SESSION['role'] != 'admin') {
+    echo "<script>
+            alert('Akses Ditolak! Anda bukan Admin.');
+            window.location.href = 'login.php';
+            </script>";
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard Admin</title>
+</head>
+<body>
+    <h1>Selamat Datang, Admin <?php echo $_SESSION['username']; ?>!</h1>
+    <p>Ini adalah halaman khusus administrator.</p>
+    
+    <a href="logout.php">Logout</a> </body>
+</html>
