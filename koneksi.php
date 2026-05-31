d6cc520356979d94f288faaf56ea6e86f8cf7565
MUHAMMAD FAKHRI ANSHARY YUSAF <fakhrianshary@gmail.com>
2026-05-31 11:34:42 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host     = "localhost";
-$user     = "kelompok2"; // Default XAMPP/Laragon
-$password = "12345";     // Kosongkan jika default
-$db       = "db_sipelda"; // Ses_uaikan dengan nama database kamu
+$user     = "root";
+$password = "";
+$db       = "db_sipelda";
 
 
 $koneksi = mysqli_connect($host, $user, $password, $db);
@@ -10,4 +10,3 @@ $koneksi = mysqli_connect($host, $user, $password, $db);
 if (!$koneksi) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
-?>
